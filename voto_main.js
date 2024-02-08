09e68a275605d7bd38eb9ca8037ba17426157e95
VINICIUS RODRIGUES GABRIELLI <vinicius_gabrielli1@estudante.sesisenai.org.br>
2024-02-08 09:15:02 -0300

    interface de usúario

---
+++ voto_main.js
@@ -0,0 +1,11 @@
+let resposta = document.getElementById('resposta')
+let cadastrar = document.getElementById('verificar')
+
+cadastrar.addEventListener('click' , ()=>{
+    let nome = document.getElementById('nome').value 
+    let idade = Number(document.getElementById('idade').value)
+    let titulo = Number(document.getElementById('titulo').value)
+
+    
+    
+})
