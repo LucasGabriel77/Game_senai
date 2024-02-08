f12eecfa2268fbfea4db33216dc32f0ff5ede679
Vinicius <vinicius_gabrielli1@estudante.sesisenai.org.br>
2024-02-08 11:54:49 -0300

    versão final do sistema de verficação de voto 08-02-20024

--- voto_class.js
+++ voto_class.js
@@ -5,5 +5,22 @@ class Eleitor{
         this.titulo = titulo
     }
 
-    
+    verificaVoto(){
+        let idade = this.idade
+
+        if(idade<16){
+            console.log("Abaixo da idade minima para votar")
+            return "Abaixo da idade minima para votar"
+        }else if((idade>=16)&&(idade<18)){
+            console.log("idade onde o voto é facultativo")
+            return "idade onde o voto é facultativo"
+        }else if((idade>=18)&&(idade<=70)){
+            console.log("idade onde o voto é obrigatorio")
+            return "idade onde o voto é obrigatorio"
+        }else if(idade > 70){
+            console.log("idade onde o voto é facultativo")
+            return "idade onde o voto é facultativo"
+        }
+
+    }  
 }
--- voto_main.js
+++ voto_main.js
@@ -2,18 +2,27 @@ let resposta = document.getElementById('resposta')
 let cadastrar = document.getElementById('verificar')
 
 cadastrar.addEventListener('click' , ()=>{
-    let nome = document.getElementById('nome').value 
-    let idade = Number(document.getElementById('idade').value)
-    let titulo = Number(document.getElementById('titulo').value)
+    let nome = document.getElementById("nome").value 
+    let idade = Number(document.getElementById("idade").value)
+    let titulo = Number(document.getElementById("titulo").value)
 
     
     console.log(nome,idade,titulo)
 
-    const cadadao = new Eleitor()
+    const cidadao = new Eleitor()
 
     cidadao.nome = nome
     cidadao.idade = idade
     cidadao.titulo = titulo
 
-    
+    console.log("intaância do objeto ", cidadao)
+
+    let mensagem = cidadao.verificaVoto
+
+    resposta.innerHTML = ''
+    resposta.innerHTML += `O eleitor ${cidadao.nome} <br>`
+    resposta.innerHTML += `tem ${cidadao.idade} anos de idade <br>`
+    resposta.innerHTML += mensagem + '<br>'
+    resposta.style.color = "skyblue"
+    resposta.style.fontSize = "1.4ren" 
 })
